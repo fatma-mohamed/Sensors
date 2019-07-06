5a24ce96d2258b6c47555b240715667c82bb1756
Fatma Mohamed <fmohamed@entrepreware.com>
2019-07-07 00:55:29 +0200

    Add Location and Temperature modules Create started variable for presenters

--- app/src/main/java/app/sensors/activities/MainActivity.kt
+++ app/src/main/java/app/sensors/activities/MainActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import android.hardware.SensorEventListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import app.sensors.presenters.TimeAndDatePresenter
-import app.sensors.presenters.VerticalDisplacementPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 import android.hardware.SensorManager
 import android.content.Context
@@ -16,8 +14,8 @@ import android.hardware.SensorEvent
 import android.net.ConnectivityManager
 import android.widget.Toast
 import app.sensors.R
-import app.sensors.presenters.LocationPresenter
-import app.sensors.presenters.TemperaturePresenter
+import app.sensors.listeners.LocationListener
+import app.sensors.presenters.*
 import app.sensors.utils.NetworkUtils
 
 
@@ -26,6 +24,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener, NetworkUtils.Conn
     lateinit var verticalDisplacementPresenter: VerticalDisplacementPresenter
     lateinit var temperaturePresenter: TemperaturePresenter
     lateinit var locationPresenter: LocationPresenter
+    lateinit var speedPresenter: SpeedPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +37,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener, NetworkUtils.Conn
         timeAndDatePresenter = TimeAndDatePresenter(tnd_current_time, tnd_current_date,
             tnd_current_time_and_date)
         verticalDisplacementPresenter = VerticalDisplacementPresenter(details_vd, this)
-        locationPresenter = LocationPresenter(details_loc, this)
         temperaturePresenter = TemperaturePresenter(temp_celsius, temp_fahrenhit, temp_kelvin,
             getSystemService(Context.SENSOR_SERVICE) as SensorManager, this)
+        speedPresenter = SpeedPresenter(speed_km_hr, speed_mile_hr,
+            speed_meter_sec, speed_acceleration, getSystemService(Context.SENSOR_SERVICE) as SensorManager, this);
 
+
+        var listeners = arrayListOf<LocationListener>();
+        listeners.add(verticalDisplacementPresenter.verticalDisplacementModule)
+        listeners.add(temperaturePresenter.temperatureModule)
+        listeners.add(speedPresenter.speedModule)
+        locationPresenter = LocationPresenter(details_loc, this,
+            listeners)
     }
 
     override fun onPause() {
@@ -55,26 +62,37 @@ class MainActivity : AppCompatActivity(), SensorEventListener, NetworkUtils.Conn
         super.onResume()
     }
 
+    /**
+     * Start modules' runnable tasks
+     */
     fun start() {
         timeAndDatePresenter.start()
         verticalDisplacementPresenter.start()
         temperaturePresenter.start()
         locationPresenter.start()
+        speedPresenter.start()
     }
 
+    /**
+     * Stop modules' runnable tasks
+     */
     fun stop() {
         timeAndDatePresenter.stop()
         verticalDisplacementPresenter.stop()
         temperaturePresenter.stop()
         locationPresenter.stop()
+        speedPresenter.stop()
     }
 
+    /**
+     * Listen to permission requests
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == verticalDisplacementPresenter.verticalDisplacementModule.REQUEST_CHECK_SETTINGS) {
+        if (requestCode == locationPresenter.locationModule.REQUEST_CHECK_SETTINGS) {
             if (resultCode == Activity.RESULT_OK) {
-                verticalDisplacementPresenter.verticalDisplacementModule.locationUpdateState = true
-                verticalDisplacementPresenter.verticalDisplacementModule.startLocationUpdates()
+                locationPresenter.locationModule.locationUpdateState = true
+                locationPresenter.locationModule.startLocationUpdates()
             }
         }
     }
@@ -83,14 +101,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener, NetworkUtils.Conn
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 
+    /**
+     * Listen to sensor changes
+     */
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null && event.values.isNotEmpty()) {
-            var temperature = event.values[0];
-            temperaturePresenter.temperatureModule.temperature = temperature
+            if(event.sensor?.stringType!!.contains("linear_acceleration")) {
+                val alpha = 0.8f;
+                speedPresenter.speedModule.gravity = alpha * speedPresenter.speedModule.gravity + (1 - alpha) * event.values[0];
+
+                var acceleration = event.values[0] - speedPresenter.speedModule.gravity;
+                speedPresenter.speedModule.acceleration = acceleration
+            } else {
+                var temperature = event.values[0];
+                temperaturePresenter.temperatureModule.temperature = temperature
+            }
         }
     }
 
-
+    /**
+     * Listen to Network changes
+     */
     override fun onNetworkConnectionChanged(isConnected: Boolean) {
         if(!isConnected) {
             this.temperaturePresenter.stop()
